afc9f2d34488470c4be75880f5177080543b1986
Charlie <charlie.fats@gmail.com>
2016-12-05 20:25:32 -0600

    -m

--- dashboard-reddit.el
+++ dashboard-reddit.el
@@ -1,10 +1,11 @@
-(require 'dashboard)
-(add-to-list dashboard-item-generators 'reddit . dashboard-insert-reddits )
+(require 'json)
+(add-to-list 'dashboard-item-generators  '(reddits . dashboard-insert-reddit))
+(add-to-list 'dashboard-items '(reddits) t)
 
-(defun dashboard-insert-reddit-list (reddit-list list)
+(defun dashboard-insert-reddit-list (title list)
   "Render REDDIT-LIST title and items of LIST."
   (when (car list)
-    (insert reddit-list)
+    (insert title )
     (mapc (lambda (el)
 	    (setq url (nth 1 (split-string el "__")) )
 	    (setq title (nth 0 (split-string el "__")) )
@@ -20,18 +21,17 @@
 			   title
 			   ))
           list)))
-(defun dashboard-insert-reddits (list-size)
+(defun dashboard-insert-reddit (list-size)
   "Add the list of LIST-SIZE items from recently edited files."
   (if (> list-size 0 )
       (progn
 	
-	(set-face-attribute 'region nil :background "#666" :foreground "#ffffff")
-	(setq file-path "/tmp/dashboard_reddits.json")
+	(setq file-path "/tmp/dashboard_reddit.json")
 	(condition-case nil
 	    (delete-file file-path)
 	  (error nil))
 
-	(require 'json)
+
 	(url-copy-file "https://www.reddit.com/r/emacs/.json"  file-path)
 	(setq reddit-list (mapcar (lambda (entry)
 				    (format "%s__%s " (let-alist entry .data.title ) (let-alist entry .data.url )))
@@ -39,12 +39,10 @@
 				  (let-alist (json-read-file  file-path) .data.children)))
 
 
-	(mark)
 	(when (dashboard-insert-reddit-list
 	       "Recent Posts to /r/emacs:"
 	       (dashboard-subseq reddit-list 0 list-size)))	 
 	(dashboard-insert--shortcut "p" "Recent Posts:")
-	(mark-defun)
 	)
     
     ))
